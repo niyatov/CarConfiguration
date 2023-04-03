4218e3087f38aa90054f3c8a24a3dc43ef2de067
NiyatovAziz <niyatovazizbek737.com>
2023-04-03 06:18:39 +0500

    carconfiguration

---
+++ Car_Configuration/Extensions/LoggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+using TelegramSink;
+
+namespace Car_Configuration.Extensions;
+
+public static class LoggerExtensions
+{
+    public static void SerilogConfig(this WebApplicationBuilder builder)
+    {
+        var logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.TeleSink("6125388892:AAGCbqi0A2gFYaJPtxMfIZUCcCdJlHZ-kSQ", "2039076116", minimumLevel: Serilog.Events.LogEventLevel.Error)
+            .CreateLogger();
+
+        builder.Logging.AddSerilog(logger);
+    }
+}
---
+++ Car_Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Car_Configuration.Data;
+using Car_Configuration.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Configuration.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddAppDbContext(this IServiceCollection collection, ConfigurationManager configuration)
+    {
+        collection.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseLazyLoadingProxies().UseSqlServer("Server=DESKTOP-7D68517\\SQLEXPRESS;Database=Car_Configurationdb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        });
+    }
+
+    public static void AddIdentityManagers(this IServiceCollection collection)
+    {
+        collection.AddIdentity<User, UserRole>(options =>
+        {
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+        }).AddRoles<UserRole>().AddEntityFrameworkStores<AppDbContext>();
+    }
+}
